ac83ace05b1a685c664cd71652c054d20c66fdd6
TheKinAZ <thekinaz@hotmail.com>
2024-02-17 18:31:31 -0700

    Week 9 Updates

--- src/main/java/projects/Projects.java
+++ src/main/java/projects/Projects.java
@@ -1,11 +1,14 @@
 package projects;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 import projects.dao.DbConnection;
+import projects.exception.DbException;
 
 public class Projects {
 
 	public static void main(String[] args) {
 		DbConnection.getConnection();
 	}
-
 }
--- target/classes/projects-schema.sql
+++ target/classes/projects-schema.sql
@@ -3,14 +3,13 @@ DROP TABLE IF EXISTS project_category;
 DROP TABLE IF EXISTS material;
 DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS project;
-
 CREATE TABLE project
 (
    project_id INT AUTO_INCREMENT NOT NULL,
    project_name VARCHAR (128) NOT NULL,
    estimated_hours TIME,
    actual_hours TIME,
-   difficult TEXT,
+   difficulty TEXT,
    notes TEXT,
    PRIMARY KEY (project_id)
 );
